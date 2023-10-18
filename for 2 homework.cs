dab3296c018d6439e0aa89a85e47106dbf36b1ac
maftuna2001 <146752177+maftuna2001@users.noreply.github.com>
2023-10-18 14:21:56 +0500

    Add files via upload

---
+++ for 2 homework.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 4;
+            int B = 9;
+            int N = 0;
+            for (int i = 0; A <= B; A++)
+            {
+                Console.WriteLine(A); N++;
+            }
+                Console.WriteLine(N);
+
+            Console.ReadKey();
+        }
+    }
+}
